e042820d91a348ec9355dfc94d3034d305103664
LinneaSjoholm <linnea.sjoholm@live.se>
2024-11-14 18:52:41 +0100

    added style to loadingscreen. installed framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Routes from './backend/routes/routes';
+import './frontend/styles/App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <Routes />
+  <div className="App">
+    <div className="View">
+        <div className="View__iphone">
+          <div className="View__iphone-screen">
+            <div className="View__iphone-top"></div>
+            <Routes />
+            <div className="View__iphone-bottom"></div>
+          </div>
+        </div>
+      </div>
+  
     </div>
   );
 }
